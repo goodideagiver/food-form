c721c5fc780ca6e4bee1aba52ffba2736ef7a336
Karol <kariurek@gmail.com>
2022-11-22 15:58:23 +0100

    Feat: add time format validation

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { FormFields } from './formTypes'
 const initialValues: FormFields = {
   name: '',
   dishType: 'unset',
-  preparationTime: '',
+  preparationTime: '00:00:00',
   diameter: 0,
   slices_of_bread: 0,
   spiciness_scale: 1,
@@ -18,10 +18,37 @@ export const App = () => {
     formSubmitHandler(values, values.dishType)
   }
 
+  const validatePreparationTime = (value: string) => {
+    const [hours, minutes, seconds] = value.split(':')
+    const hoursInt = parseInt(hours, 10)
+    const minutesInt = parseInt(minutes, 10)
+    const secondsInt = parseInt(seconds, 10)
+
+    if (!value) return 'Required'
+
+    if (!value.match(/^([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/))
+      return 'Please use HH:MM:SS time format'
+
+    if (hoursInt < 0 || hoursInt > 23) {
+      return 'Hours must be between 0 and 23'
+    }
+
+    if (minutesInt < 0 || minutesInt > 59) {
+      return 'Minutes must be between 0 and 59'
+    }
+
+    if (secondsInt < 0 || secondsInt > 59) {
+      return 'Seconds must be between 0 and 59'
+    }
+
+    return undefined
+  }
+
   return (
     <Formik initialValues={initialValues} onSubmit={onFormSubmit}>
       {(props: FormikProps<FormFields>) => {
         const { dishType, name } = props.values
+        const errors = props.errors
         const nameIsSet = name && name.trim().length > 0
 
         const submitDisabled = !nameIsSet || dishType === 'unset'
@@ -34,6 +61,14 @@ export const App = () => {
                 <label>
                   <span>Dish name:</span>
                   <Field name='name' />
+                </label>
+                <label>
+                  <span>Preparation time:</span>
+                  <Field
+                    validate={validatePreparationTime}
+                    name='preparationTime'
+                  />
+                  {errors.preparationTime && <p>{errors.preparationTime}</p>}
                 </label>
                 <label>
                   <span>Dish type:</span>
